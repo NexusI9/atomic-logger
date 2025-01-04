76f27dc4e7e2edd6946f30d7db705b6d2250e06a
NexusI9 <nassim.elkhantour@gmail.com>
2025-01-04 16:01:24 +0800

    switch approach

--- include/plog.h
+++ include/plog.h
@@ -1,63 +1,55 @@
 #include <fcntl.h>
 #include <stdatomic.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
 #define LOG_FILE "log.log"
 #define LINE_SIZE 64
 #define MAX_LINES 8192
-#define LOG_CAPACITY (512 * 1024 + 4)
+#define LOG_FILE_CAPACITY (1024 * 1024)
 
-
-typedef struct{
-
-
+typedef struct {
+  const atomic_uint *time;
+  const char *file;
+  const unsigned int line;
+  const char *message;
 } LogLine;
 
-// initialize log file
-int init_log_file(const char *filename, size_t size) {
-  int fd = open(filename, O_RDWR | O_CREAT, 0666);
+atomic_uint *counter = NULL;
+FILE *log_file = NULL;
 
-  if (fd == -1) {
-    perror("Failed to open file");
-    return fd;
-  } else if (ftruncate(fd, size) == -1) {
-    perror("Failed to set log file size");
-    close(fd);
-    return -1;
+// initialize log file, make sure exists (or create) and own enough capacity
+void init_log_file(const char *filename) {
+
+  if (log_file != NULL) {
+    return; // already init
   }
 
-  close(fd);
-  return 0;
+  // open potential existing file
+  log_file = fopen(filename, "w+");
+  if (!log_file) {
+    perror("Couldn't open log file");
+    exit(1);
+  }
+
+  // init atomic counter and map to last 4 bytes of memory map
+  int fd = fileno(log_file);
+  counter = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+                 LOG_FILE_CAPACITY - 4);
+  if (counter == MAP_FAILED) {
+    perror("Couldn't map atomic counter in memory");
+    exit(1);
+  }
 }
 
-// initialize atomic counter
-atomic_uint *map_atomic_counter(const char *filename) {
+void log_c(const char *message) {
 
-  int fd = open(filename, O_RDWR);
-  if (fd == -1) {
-    return NULL;
+  if (!log_file || !counter) {
+    perror("log not initialized, make sure to call init_log_file() function prior logging");
+    return;
   }
 
-  // Map counter to last 4 bytes
-  void *file_in_memory =
-      mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (512 * 1024));
-  close(fd);
-
-  if (file_in_memory == MAP_FAILED) {
-    perror("failed to map file in memory");
-    return NULL;
-  }
-
-  return (atomic_uint *)file_in_memory;
-}
-
-
-void increment_counter(atomic_uint* counter){
-    if(!counter){
-        fprintf(stderr, "Counter not mapped");
-        return;
-    }
-    unsigned int next_index = atomic_fetch_add(counter, 1);
+  atomic_fetch_add(counter, 1);
 }
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include "include/plog.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
-
-    printf("Test\n");
-
+int main(int argc, char *argv[]) {
+  init_log_file("logfile.log");
+  printf("Test\n");
+  log_c("First message");
 
 }
